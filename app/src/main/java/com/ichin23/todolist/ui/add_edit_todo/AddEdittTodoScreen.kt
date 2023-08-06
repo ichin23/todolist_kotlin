c42f533eec0c61577b56ce590b9fa4b0b69c5500
ichin23 <pedroferreiramanoel@gmail.com>
2023-08-06 00:24:11 -0300

    fix warnings

--- app/src/main/java/com/ichin23/todolist/ui/add_edit_todo/AddEdittTodoScreen.kt
+++ app/src/main/java/com/ichin23/todolist/ui/add_edit_todo/AddEdittTodoScreen.kt
@@ -1,6 +1,5 @@
 package com.ichin23.todolist.ui.add_edit_todo
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,26 +14,19 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ichin23.todolist.util.UiEvent
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -44,7 +36,7 @@ fun AddEditTodoScreen(
     viewModel: AddEditTodoViewModel = hiltViewModel()
 ) {
 
-    val snackbarHostState = remember { SnackbarHostState() }
+    val snackBarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(key1 = true) {
@@ -53,7 +45,7 @@ fun AddEditTodoScreen(
                 is UiEvent.PopBackStack -> onPopBackStack()
                 is UiEvent.ShowSnackBar -> {
                     coroutineScope.launch {
-                        snackbarHostState.showSnackbar(
+                        snackBarHostState.showSnackbar(
                             message = event.message,
                             actionLabel = event.action
                         )
